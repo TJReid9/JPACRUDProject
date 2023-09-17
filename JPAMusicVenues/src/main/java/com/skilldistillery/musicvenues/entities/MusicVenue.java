3b2229548c0c1acf3f555a6f1ceb3c48ac1afa9d
TJReid9 <tjreid20@icloud.com>
2023-09-17 04:42:40 -0600

    Stub MusicVenue entity, test hibernate configeration

---
+++ JPAMusicVenues/src/main/java/com/skilldistillery/musicvenues/entities/MusicVenue.java
@@ -0,0 +1,61 @@
+package com.skilldistillery.musicvenues.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "music_venue")
+public class MusicVenue {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@Column(name = "image_url")
+	private String imageURL;
+
+	public MusicVenue() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getImageURL() {
+		return imageURL;
+	}
+	
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MusicVenue [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
+	
+	
+	
+
+}
---
+++ JPAMusicVenues/src/test/java/com/skilldistillery/musicvenues/entities/MusicVenueTest.java
@@ -0,0 +1,58 @@
+package com.skilldistillery.musicvenues.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MusicVenueTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private MusicVenue venue;
+
+	public MusicVenue getVenue() {
+		return venue;
+	}
+
+	public void setVenue(MusicVenue venue) {
+		this.venue = venue;
+	}
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMusicVenues");
+		
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		venue = em.find(MusicVenue.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		venue = null;
+	}
+
+	@Test
+	void test_MusicVenue_entity_mapping() {
+		assertNotNull(venue);
+		assertEquals("Mishawaka Amphitheater", venue.getName());
+	}
+
+}
